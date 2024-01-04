95d6936f62fac96bef4c61b1b3fcb194f6d18236
Wildan <149742270+wildanthegoat@users.noreply.github.com>
2024-01-04 10:31:53 +0800

    sidebar.blade.php

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -1,7 +1,7 @@
 <div class="main-sidebar sidebar-style-2">
     <aside id="sidebar-wrapper">
         <div class="sidebar-brand">
-            <a href="index.html">SIMPADU-C030322155</a>
+            <a href="index.html">SIMPADU-C030322156</a>
         </div>
         <div class="sidebar-brand sidebar-brand-sm">
             <a href="index.html">SKD</a>
