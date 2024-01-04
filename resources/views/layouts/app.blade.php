6dbbd02592690fc2536717a47dc25259c1dced73
Wildan <149742270+wildanthegoat@users.noreply.github.com>
2024-01-04 10:53:37 +0800

    app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no"
         name="viewport">
-    <title>@yield('title') &mdash; Simpadu-C030322138</title>
+    <title>@yield('title') &mdash; Simpadu-C030322156</title>
 
     <!-- General CSS Files -->
     <link rel="stylesheet"
